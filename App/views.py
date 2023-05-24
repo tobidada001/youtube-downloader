e1fed648eda991e35e3f8f74e93b89c8ea6d1503
OLUWATOBI <dadaoluwatobijohn@gmail.com>
2023-05-24 06:00:51 +0100

    I added Ajax to perform the form submission

--- App/views.py
+++ App/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from pytube import YouTube
 
 # Create your views here.
 def index(request):
+
+    if request.method == 'POST':
+        link = request.POST['link']
+        
+        # youtubeObject = YouTube(link)
+        # youtubeObject = youtubeObject.streams.get_highest_resolution()
+        # try:
+        #     youtubeObject.download()
+        # except:
+        #     print("An error has occurred")
+        # print("Download is completed successfully")
+
 
     return render(request, 'index.html')
